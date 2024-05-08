2086f0d3a3f9a9b913b3c22d7a9a300d9a47b60d
snowrab <freecreate@protonmail.com>
2024-05-08 14:54:59 +0800

    增加输出全部scene对话的功能

--- krkr_utils/diag2gptvit.py
+++ krkr_utils/diag2gptvit.py
@@ -31,19 +31,25 @@ def read_file_to_list(filename):
     return data
 
 
-def filter_and_sample(data, k, n, cv, random_number):
+def filter_and_sample(data, k=0, n=1, cv=None, random_number=1, sample = True):
     random.seed(random_number)
-    filtered_data = [item for item in data if item['voice'] is not None and len(item['content']) >= k and item['user']==cv]
-
+    if cv:
+        filtered_data = [item for item in data if item['voice'] is not None and len(item['content']) >= k and item['user']==cv]
+    else:
+        filtered_data = [item for item in data if item['voice'] is not None and len(item['content']) >= k]
     user_dict = {}
 
     for item in filtered_data:
         user_dict.setdefault(item['user'], []).append(item)
 
     sampled_data = []
-    for user, items in user_dict.items():
-        sampled_items = random.sample(items, min(len(items), n))  # 防止n大于用户字典长度
-        sampled_data.extend(sampled_items)
+    if sample:
+        for user, items in user_dict.items():
+            sampled_items = random.sample(items, min(len(items), n))  # 防止n大于用户字典长度
+            sampled_data.extend(sampled_items)
+    else:
+        for user, items in user_dict.items():
+            sampled_data.extend(items)
 
     return sampled_data
 
@@ -74,13 +80,14 @@ def filter_sample_and_copy(data, k, n, random_number,voice_dir,cv,outdir):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="预处理生成Bert-vits2模型的输入数据")
-    parser.add_argument('-sd', '--scene_dir', type=str, default="", help="scene文本路径")
+    parser.add_argument('-sp', '--scene_path', type=str, default="", help="scene文本路径")
     parser.add_argument('-r', '--random_seed', type=int, default=1, help="随机种子")
     parser.add_argument('-l', '--min_length', type=int, default=20, help='最小对话长度')
     parser.add_argument('-s', '--size', type=int, default=40, help='采样数')
-    parser.add_argument('-vd', '--voice_dir', type=str, default="", help="wav文件路径")
+    parser.add_argument('-vd', '--voice_dir', type=str, default="", help="wav文件夹路径")
     parser.add_argument('--cv', type=str, default="乃愛", help="参考音频角色")
     parser.add_argument('--out', type=str, default='../processed/data/', help='The path to store wav files and their list.')
+    parser.add_argument("-n", "--name", type=str, default="", help="转化模式：只将scene格式化为list")
     args = parser.parse_args()
 
     
@@ -90,6 +97,11 @@ if __name__ == "__main__":
 
 
 
-    data = read_file_to_list(args.scene_dir)
+    data = read_file_to_list(args.scene_path)
     print(type(data[0]))
-    filter_sample_and_copy(data, args.min_length, args.size, args.random_seed,args.voice_dir, cv = args.cv, outdir = outdir)
+    if args.name:
+        all_data = filter_and_sample(data,sample=False)
+        file_path = os.path.join(outdir,args.name+".list")
+        write_to_file(all_data,file_path)
+    else:
+        filter_sample_and_copy(data, args.min_length, args.size, args.random_seed,args.voice_dir, cv = args.cv, outdir = outdir)
